3d3a0cefc57f2dc57f0e57ae3c389fe63f07873e
Elena1505 <124672713+Elena1505@users.noreply.github.com>
2023-08-14 12:37:30 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -5,6 +5,11 @@ import pickle
 
 app = Flask(__name__)
 data = pd.read_csv("data.csv")
+model_name = "LGBMClassifier"
+model_version = 4
+best_model = mlflow.sklearn.load_model(model_uri=f"models:/{model_name}/{model_version}")
+with open("best_threshold.pickle", "rb") as f:
+    thres = pickle.load(f)
 
 
 def predict(index, data, best_model, thres):
@@ -24,8 +29,7 @@ def prediction():
     try:
         index = request.get_json()
         print(index)
-        #index = int(index["value"])
-        index = 0
+        index = int(index["value"])
         data_index = data.iloc[[index]]
         rep, proba = predict(index, data_index, best_model, thres)
         d = {'rep': rep, 'proba': proba}
@@ -35,9 +39,4 @@ def prediction():
 
 
 if __name__ == "__main__":
-    model_name = "LGBMClassifier"
-    model_version = 4
-    best_model = mlflow.sklearn.load_model(model_uri=f"models:/{model_name}/{model_version}")
-    with open("best_threshold.pickle", "rb") as f:
-        thres = pickle.load(f)
     app.run(debug=True)
